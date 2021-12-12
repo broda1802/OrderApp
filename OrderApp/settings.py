0c8bb54105783fdd9c67db130727e8274b6c45f4
broda1802 <bebe1802@o2.pl>
2021-12-12 20:58:27 +0100

    logo

--- OrderApp/settings.py
+++ OrderApp/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'accounts/static')]
+MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
