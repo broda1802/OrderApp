0b3934b8a7bdf8d7c6c8b35f94ea751cb6be5407
broda1802 <bebe1802@o2.pl>
2021-12-14 23:25:32 +0100

    delete,order

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
-
 from django.urls import path
-from . import views
+from .views import HomeView, ProductView, CustomerView, CreateOrderView, UpdateOrderView, DeleteOrderView
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
-    path('customer/<int:pk>/', views.customer, name='customer'),
+    path('', HomeView.as_view(), name='home'),
+    path('products/', ProductView.as_view(), name='products'),
+    path('customer/<int:pk>/', CustomerView.as_view(), name='customer'),
+    path('create_order/', CreateOrderView.as_view(), name='crate_order'),
+    path('update_order/<int:pk>/', UpdateOrderView.as_view(), name='update_order'),
+    path('delete_order/<int:pk>/', DeleteOrderView.as_view(), name='delete_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,38 +1,103 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderForm
 
 # Create your views here.
+from django.views import View
+
 from accounts.models import Product, Order, Customer
 
 
-def home(request):
-    orders = Order.objects.all()
-    customers = Customer.objects.all()
-    total_customers = customers.count()
-    total_orders = orders.count()
-    delivered = orders.filter(status='Delivered').count()
-    pending = orders.filter(status='Pending').count()
-    context = {'orders': orders,
-               'customers': customers,
-               'total_orders': total_orders,
-               'total_customers': total_customers,
-               'delivered': delivered,
-               'pending': pending
-               }
-    return render(request, 'accounts/dashboard.html', context)
+class HomeView(View):
+
+    def get(self, request):
+        orders = Order.objects.all()
+        customers = Customer.objects.all()
+        total_customers = customers.count()
+        total_orders = orders.count()
+        delivered = orders.filter(status='Delivered').count()
+        pending = orders.filter(status='Pending').count()
+        context = {'orders': orders,
+                   'customers': customers,
+                   'total_orders': total_orders,
+                   'total_customers': total_customers,
+                   'delivered': delivered,
+                   'pending': pending
+                   }
+        return render(request, 'accounts/dashboard.html', context)
 
 
-def products(request):
-    products = Product.objects.all()
-    return render(request, 'accounts/products.html', {'products': products})
+class ProductView(View):
+
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    orders = customer.order_set.all()
-    order_count = orders.count()
-    context = {'customer': customer,
-               'orders': orders,
-               'order_count': order_count
-               }
-    return render(request, 'accounts/customer.html', context)
+class CustomerView(View):
+
+    def get(self, request, pk):
+        customer = Customer.objects.get(id=pk)
+        orders = customer.order_set.all()
+        order_count = orders.count()
+        context = {'customer': customer,
+                   'orders': orders,
+                   'order_count': order_count
+                   }
+        return render(request, 'accounts/customer.html', context)
+
+
+class CreateOrderView(View):
+
+    def get(self, request):
+        form = OrderForm()
+        context = {'form': form
+                   }
+        return render(request, 'accounts/order_form.html', context)
+
+    def post(self, request):
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+        context = {'form': form
+                   }
+        return render(request, 'accounts/order_form.html', context)
+
+
+class UpdateOrderView(View):
+
+    def get(self, request, pk):
+        order = Order.objects.get(id=pk)
+        form = OrderForm(instance=order)
+        context = {'form': form}
+        return render(request, 'accounts/order_form.html', context)
+
+    def post(self, request, pk):
+        order = Order.objects.get(id=pk)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+        context = {'form': form
+                   }
+        return render(request, 'accounts/order_form.html', context)
+
+
+class DeleteOrderView(View):
+    def get(self, request, pk):
+        order = Order.objects.get(id=pk)
+        context = {'order': order
+                   }
+        return render(request, 'accounts/delete.html', context)
+
+    def post(self, request, pk):
+        order = Order.objects.get(id=pk)
+        order.delete()
+        return redirect('home')
+
+
+
+
+
